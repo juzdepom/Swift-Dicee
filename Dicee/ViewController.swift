9d3b3ff9a9fb839324585051eb327ce5772b9dbf
Julia Gao Miller <julia.gao.miller@gmail.com>
2018-02-09 07:28:12 -0500

    Rolling dice creates random dice images.

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -9,17 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var randomDiceIndex1: Int = 0;
+    var randomDiceIndex2: Int = 0;
+    
+    let diceArray = [#imageLiteral(resourceName: "dice2"), #imageLiteral(resourceName: "dice3"), #imageLiteral(resourceName: "dice4"), #imageLiteral(resourceName: "dice5"), #imageLiteral(resourceName: "dice6")]
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        updateDiceImages()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        updateDiceImages()
+    }
+    
+    func updateDiceImages(){
+        randomDiceIndex1 = Int(arc4random_uniform(4))
+        randomDiceIndex2 = Int(arc4random_uniform(4))
+        
+        diceImageView1.image = diceArray[randomDiceIndex1]
+        diceImageView2.image = diceArray[randomDiceIndex2]
+        
     }
 
-
+    
 }
 
